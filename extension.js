d2a6a1ce32b11a3f32a218151a2ca04f796ff4ca
Gaurav Mali <hello@gauravmali.com>
2011-12-01 02:33:02 -0600

    jQueryfied it

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-(function() {
+$(document).ready(function(){
 
     var PERMS_URL_PARAM = "perms",
         PERMS_FORM_PARAM = "perms",
@@ -15,12 +15,14 @@
         }
         return dict;
     };
+
     function get_active_permissions() {
         var parts = split_url_parts()
         var perms = parts[PERMS_URL_PARAM]
         if(perms) return perms.split(",");
         else return [];
     };
+    
     function set_perms_in_form(choosen) {
         /*
         * Modify the form live instead of reloading the page.
@@ -40,46 +42,29 @@
             new_params.push((enc(key) + "=" + enc(parts[key])));
         }
         window.location.replace(url + "?" + new_params.join("&"));
-    };
-
-    function generate_header() {
-
-        /*
-        * FIXME: some kind of proper templating or something here.
-        * Maybe find a lightweight js lib for DOM manipulation.
-        * Or just use jQuery.
-        */
-        
-        var holder = document.createElement('div');
-        holder.className = "fPrivacyHeader";
-        holder.innerHTML = "";
-        
-        var permissions = get_active_permissions();
-        
-        for(var p in permissions) {
-            var perm = permissions[p];
-            holder.innerHTML += '<input type="checkbox" id="'+perm+'" value="'+perm+'" checked=checked name="newperms" /><label for="'+perm+'">'+perm+'</label>';
-        }
-
-        var btn = document.createElement("button");
-        btn.appendChild(document.createTextNode("Update"));
-        btn.onclick = function() {
-            // "Update" is clicked: figure out what's checked and reload the window.
-            var elements = document.getElementsByName("newperms");
-            var permissions = Array.prototype.slice.call(elements);
-            permissions = permissions.filter(function(me){ return me.checked });
-            permissions = permissions.map(function(me){ return me.value;});
-            reload_page(permissions);
-        }
-        
-        holder.innerHTML += '<a target="_new" href="https://www.facebook.com/settings?tab=applications">Application Settings</a>';
-        
-        // put it in the pizza!
-        document.body.insertBefore(holder, document.body.firstChild);
-        holder.appendChild(btn);
     }
 
-    generate_header();
-    console.log(get_active_permissions());
+    $("body").prepend('<div class="fPrivacyHeader"></div>');
 
-})();
+    var permissions = get_active_permissions();
+
+    for(var p in permissions) {
+        var perm = permissions[p];
+        $(".fPrivacyHeader").append('<input type="checkbox" id="'+perm+'" value="'+perm+'" checked=checked name="newperms" /><label for="'+perm+'">'+perm+'</label>');
+    }
+    
+    $(".fPrivacyHeader").append('<a target="_new" href="https://www.facebook.com/settings?tab=applications">Application Settings</a>');
+    $(".platform_dialog_buttons").prepend('<button class="fPrivacy uiButton uiButtonLarge">Update</button>');
+
+    $('button.fPrivacy').click(function(e) {
+        // "Update" is clicked: figure out what's checked and reload the window.
+        var elements = document.getElementsByName("newperms");
+        var permissions = Array.prototype.slice.call(elements);
+        permissions = permissions.filter(function(me){ return me.checked });
+        permissions = permissions.map(function(me){ return me.value;});
+        reload_page(permissions);
+
+        e.preventDefault();
+    });
+
+});
